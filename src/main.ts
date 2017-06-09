f9f655871ff078f1f217c7c623bca47e7abc43fc
Raiden Dilan <raiden18@me.com>
2017-06-09 20:21:02 +0100

    basic concept of  handling properties.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import Vue from 'vue';
+import {HelloComponent} from './hello';
 
 new Vue({
   el: '#root',
-  template: '<h1>{{message}}</h1>',
+  template: `
+    <div>
+      <h1>{{message}}</h1>
+      <hello v-model="message"/>
+    </div>
+    `,
+    components: {
+      hello: HelloComponent,
+    },
   data: {
     message: 'Hello from Vue.js'
-  }
+  },
 });
